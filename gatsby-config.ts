4ffe3915cb935cf27d1f2f367c8987ac0dfd04e7
omardeangelis <omar.hpvfilm@gmail.com>
2022-09-18 16:05:26 +0200

    configurazione avanzata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,40 +1,59 @@
 import type { GatsbyConfig } from "gatsby";
+import { config as dotenv } from "dotenv";
+
+dotenv({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `groomy-gatsby`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Groomy`,
+    siteUrl: `https://www.yourdomain.tld`,
+    metaDescription:
+      "Impara dai migliori chef italiani ricette orientali, funzionali ed estive",
+    author: "@hpv4learning",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-sanity',
-    options: {
-      "projectId": "7rtujyv5",
-      "dataset": "production"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        "projectId": process.env.GATSBY_SANITY_PROJECT_ID,
+        "dataset": process.env.GATSBY_SANITY_DATASET,
+      },
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        "icon": "src/images/icon.png",
+      },
     },
-    __key: "pages"
-  }]
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        "name": "images",
+        "path": "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        "name": "pages",
+        "path": "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
 };
 
 export default config;
