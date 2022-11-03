edf9554052e245f0a9f7e1a6bb1ebbbd6ceab69e
omardeangelis <omar.hpvfilm@gmail.com>
2022-11-03 23:37:25 +0100

    types and default last recipe ID

---
+++ query.ts
@@ -0,0 +1,28 @@
+// export type CategoryQueryProps = {
+//   data: {
+//     allSanityCategory: {
+//       nodes: {
+//         titolo: NonNullable<Queries.SanityCategory["titolo"]>;
+//         _id: NonNullable<Queries.SanityCategory["_id"]>;
+//         ricettario: {
+//           _id: NonNullable<Queries.SanityRecipe["_id"]>;
+//           _createdAt: NonNullable<Queries.SanityRecipe["_createdAt"]>;
+//         }[];
+//       }[];
+//     };
+//   };
+// };
+
+// export type AllRecipeQueryProps = {
+//   data: {
+//     allSanityRecipe: {
+//       nodes: {
+//         titolo: NonNullable<Queries.SanityRecipe["titolo"]>;
+//         id: NonNullable<Queries.SanityRecipe["id"]>;
+//         category: {
+//           titolo: NonNullable<Queries.SanityRecipe["category"]>["titolo"];
+//         };
+//       }[];
+//     };
+//   };
+// };
--- src/feature/category/LastRecipe.tsx
+++ src/feature/category/LastRecipe.tsx
@@ -9,7 +9,96 @@ const Wrapper = styled("div")<any>({
 });
 
 const LastRecipe = () => {
-  return <Wrapper></Wrapper>;
+  return (
+    <></>
+    // <Wrapper>
+    //   <div
+    //     style={{
+    //       position: "absolute",
+    //       top: "0px",
+    //       left: "0px",
+    //       bottom: "0px",
+    //       right: "0px",
+    //     }}
+    //   >
+    //     <div
+    //       className='flex flex-column justify-content-center'
+    //       style={{
+    //         background: theme.colors.yellow[400],
+    //         maxWidth: "50%",
+    //         width: "100%",
+    //         height: "100%",
+    //       }}
+    //     />
+
+    //     <div
+    //       className='flex flex-column justify-content-center'
+    //       style={{
+    //         width: "100%",
+    //       }}
+    //     />
+    //   </div>
+    //   <Container
+    //     style={{
+    //       zIndex: 2,
+    //       display: "flex",
+    //       justifyContent: "space-between",
+    //     }}
+    //   >
+    //     <div
+    //       className='flex flex-column justify-content-center'
+    //       style={{
+    //         background: theme.colors.yellow[400],
+    //         width: "100%",
+    //         height: "100%",
+    //       }}
+    //     >
+    //       <div style={{}}>
+    //         <Display weight='semibold'>{titolo}</Display>
+    //         <div
+    //           className='spacer-md'
+    //           style={{
+    //             maxWidth: "596px",
+    //           }}
+    //         >
+    //           <Heading>{riassunto}</Heading>
+    //         </div>
+    //         <div className='spacer-lg'>
+    //           <Link to={`/${slug}/${createSlugFromTitle(titolo)}/`}>
+    //             <Button color='orange' size='xl'>
+    //               Scropri di più
+    //             </Button>
+    //           </Link>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div
+    //       className='flex flex-column justify-content-center'
+    //       style={{
+    //         width: "100%",
+    //         height: "100%",
+    //         alignItems: "flex-end",
+    //       }}
+    //     >
+    //       {gastbyImage ? (
+    //         <div>
+    //           <RoundedImageContainer>
+    //             <GatsbyImage
+    //               image={gastbyImage}
+    //               alt={titolo || "Last Recipe Image"}
+    //               loading='eager'
+    //               style={{
+    //                 height: "100%",
+    //                 width: "100%",
+    //               }}
+    //             />
+    //           </RoundedImageContainer>
+    //         </div>
+    //       ) : null}
+    //     </div>
+    //   </Container>
+    // </Wrapper>
+  );
 };
 
 export default LastRecipe;
